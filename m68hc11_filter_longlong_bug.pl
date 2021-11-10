3662b2e738291ea5933130506cb33f5f8fa14982
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-11-11 07:33:34 +0900

    up

---
+++ m68hc11_filter_longlong_bug.pl
@@ -0,0 +1,9 @@
+#! /usr/bin/env perl
+
+while (<>) {
+  chop;
+  if (wdog.exe : /home/khasegawa/lang/41_ISO_IEC_9899/6_Language/1_Lexical_elements/3_Constants/2_Integer_constants/1_ok:diff test000.log test000.lof) {
+     next;
+  }
+  print $_, "\n";
+}
